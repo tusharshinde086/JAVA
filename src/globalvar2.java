0d1d2ea884c67feb81324eb0922cf65f3c96c3cb
Tushar Shinde 086 <tusharshindets086@gmail.com>
2024-08-16 01:04:29 -0700

    java commit

--- src/globalvar2.java
+++ src/globalvar2.java
@@ -1,5 +1,6 @@
 
 public class globalvar2 {
+    static a = 3345;
     int x = 5540;  
     public static void main(String[] args){
           System.out.println(x);
---
+++ src/globalvar6.java
@@ -0,0 +1,12 @@
+public class globalvar6 {
+   
+        static int a = 5540;  
+         int b = 234;
+     
+         public static void main(String[] args){
+            
+               System.out.println(globalvar5,b);
+         
+     }
+     }
+
